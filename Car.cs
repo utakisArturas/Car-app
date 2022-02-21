26dee39b063b177bbd598941f07b7098f651ff02
utakisArturas <utakis.arturas.github@gmail.com>
2022-02-21 19:12:26 +0200

    car app
    push

---
+++ Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CA0221
+{
+    public class Car : IVehicle
+    {
+        public string Model { get; set; }
+        public int Fuel { get; set; }
+        public int GasTankCapacity { get; set; }
+
+        public Car(string model, int fuel, int gasTankCapacity)
+        {
+            Model = model;
+            Fuel = fuel;
+            GasTankCapacity = gasTankCapacity;
+        }
+
+        public void Drive() 
+        {
+            if (Fuel > 0)
+            {
+                Console.WriteLine("You can drive");
+            }
+            else
+            {
+                Console.WriteLine("Gas tank is empty, refuel");
+            }
+
+        }
+
+        public void Refuel(int liters)
+        {
+            if (liters> 0 && liters <= GasTankCapacity)
+            {
+                Fuel = liters;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CA0221
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car("Mazda",-5,76);
+            car.Drive();
+            car.Refuel(15);
+            car.Drive();
+        }
+    }
+}
